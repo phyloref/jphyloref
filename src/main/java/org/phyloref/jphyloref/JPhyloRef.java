84d6687ec6b4d819ccb5931740d944089d320c09
gaurav <gaurav@ggvaidya.com>
2017-09-25 18:24:57 -0400

    Initial commit.

---
+++ src/main/java/org/phyloref/jphyloref/JPhyloRef.java
@@ -0,0 +1,96 @@
+package org.phyloref.jphyloref;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.phyloref.jphyloref.commands.Command;
+
+/**
+ * Main class for JPhyloRef. Figures out what the user 
+ * wants us to do. 
+ *
+ */
+public class JPhyloRef 
+{
+	public static final String VERSION = "0.0.1-SNAPSHOT";
+	
+	private List<Command> commands = Arrays.asList(
+		(Command) new HelpCommand()
+	);
+	
+    public static void main( String[] args )
+    {
+        JPhyloRef jphyloref = new JPhyloRef();
+        jphyloref.execute(args);
+    }
+    
+    public void execute(String[] args) {
+    	System.out.println("jphyloref/" + VERSION + "\n");
+        
+        // Prepare to parse command line arguments.
+        Options opts = new Options();
+        for(Command cmd: commands) {
+        	cmd.addCommandLineOptions(opts);
+        }
+        
+        // Parse command line arguments.
+        CommandLine cmdLine;
+        try {
+        	cmdLine = new DefaultParser().parse(opts, args);
+        } catch(ParseException ex) {
+        	System.err.println("Could not parse command line options: " + ex);
+        	System.exit(1);
+        	return;
+        }
+        
+        // The first unprocessed argument should be the command.
+        if(cmdLine.getArgList().isEmpty()) {
+        	// No command provided! Activate help.
+        	HelpCommand help = new HelpCommand();
+        	help.execute(cmdLine);
+        } else {
+        	String command = cmdLine.getArgList().get(0);
+        	
+        	for(Command cmd: commands) {
+        		if(cmd.getName().equalsIgnoreCase(command)) {
+        			// match!
+        			cmd.execute(cmdLine);
+        			System.exit(0);
+        			return;
+        		}
+        	}
+        	
+        	System.err.println("Error: command '" + command + "' has not been implemented.");
+        	System.exit(1);
+        	return;
+        }
+    }
+    
+    private class HelpCommand implements Command {
+		public String getName() { return "help"; }
+		public String getDescription() { return "Provides help on all jphyloref commands"; }
+		public void addCommandLineOptions(Options opts) { }
+		public void execute(CommandLine cmdLine) {
+			// No arguments are provided.
+			
+			System.err.println("Synopsis: jphyloref <command> <options>\n");
+			
+			System.err.println("Where command is one of:");
+			for(Command cmd: commands) {
+				System.err.println(" - " + cmd.getName() + ": " + cmd.getDescription());
+				
+				Options opts = new Options();
+				cmd.addCommandLineOptions(opts);
+				
+				for(Option opt: opts.getOptions()) {
+					System.err.println("\t " + opt.getDescription());
+				}
+			}
+		}
+    }
+}
