d3c9a40b8867a508e2e93ebf142decee6b26757a
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-11-14 19:11:20 -0500

    Added tests for PhylorefHelper.getStatusesForPhyloref().

--- src/test/java/org/phyloref/jphyloref/helpers/PhylorefHelperTest.java
+++ src/test/java/org/phyloref/jphyloref/helpers/PhylorefHelperTest.java
@@ -2,6 +2,7 @@ package org.phyloref.jphyloref.helpers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,15 +11,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.phyloref.jphyloref.helpers.PhylorefHelper.PhylorefStatus;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.vocab.OWL2Datatype;
 import uk.ac.manchester.cs.jfact.JFactFactory;
 
 /** A unit test for the PhylorefHelper class. */
@@ -57,10 +62,100 @@ class PhylorefHelperTest {
           df.getOWLAnnotationAssertionAxiom(
               df.getRDFSLabel(), phyloref1IRI, df.getOWLLiteral("Test phyloreference", "en")));
 
-      // TODO: Set up annotation properties for setting statuses.
+      // Set up annotation properties for setting statuses.
+      axioms.addAll(
+          createStatus(
+              df,
+              phyloref1IRI,
+              IRI.create("http://purl.org/spar/pso/draft"),
+              Instant.parse("2018-11-14T01:00:00.00Z"),
+              Instant.parse("2018-11-14T02:00:00.00Z")));
+
+      axioms.addAll(
+          createStatus(
+              df,
+              phyloref1IRI,
+              IRI.create("http://purl.org/spar/pso/final-draft"),
+              Instant.parse("2018-11-14T02:00:00.00Z"),
+              Instant.parse("2018-11-14T03:00:00.00Z")));
+
+      axioms.addAll(
+          createStatus(
+              df,
+              phyloref1IRI,
+              IRI.create("http://purl.org/spar/pso/submitted"),
+              Instant.parse("2018-11-14T03:00:00.00Z"),
+              Instant.parse("2018-11-14T04:00:00.00Z")));
+
+      axioms.addAll(
+          createStatus(
+              df,
+              phyloref1IRI,
+              IRI.create("http://purl.org/spar/pso/published"),
+              Instant.parse("2018-11-14T04:00:00.00Z"),
+              null));
 
       // Set up the test ontology.
       testOntology = ontologyManager.createOntology(new HashSet<>(axioms));
+    }
+
+    /**
+     * Return an OWLAnonymousIndividual (i.e. an OWLAnnotationObject) that can be the target of a
+     * `pso:withStatus`.
+     */
+    private List<OWLAxiom> createStatus(
+        OWLDataFactory df, IRI phylorefIRI, IRI status, Instant startTime, Instant endTime) {
+      // Create list of axioms.
+      List<OWLAxiom> axioms = new ArrayList<>();
+
+      // Add start time interval to a timeInterval anonymous individual.
+      OWLAnonymousIndividual timeInterval = df.getOWLAnonymousIndividual();
+      if (startTime != null) {
+        axioms.add(
+            df.getOWLAnnotationAssertionAxiom(
+                df.getOWLAnnotationProperty(
+                    IRI.create(
+                        "http://www.ontologydesignpatterns.org/cp/owl/timeinterval.owl#hasIntervalStartDate")),
+                timeInterval,
+                df.getOWLLiteral(startTime.toString(), OWL2Datatype.XSD_DATE_TIME)));
+      }
+
+      // Add end time interval to a timeInterval anonymous individual.
+      if (endTime != null) {
+        axioms.add(
+            df.getOWLAnnotationAssertionAxiom(
+                df.getOWLAnnotationProperty(
+                    IRI.create(
+                        "http://www.ontologydesignpatterns.org/cp/owl/timeinterval.owl#hasIntervalEndDate")),
+                timeInterval,
+                df.getOWLLiteral(endTime.toString(), OWL2Datatype.XSD_DATE_TIME)));
+      }
+
+      // Add timeInterval to a holdsStatusInTime anonymous individual.
+      OWLAnonymousIndividual holdsStatusInTime = df.getOWLAnonymousIndividual();
+      axioms.add(
+          df.getOWLAnnotationAssertionAxiom(
+              df.getOWLAnnotationProperty(
+                  IRI.create("http://www.essepuntato.it/2012/04/tvc/atTime")),
+              holdsStatusInTime,
+              timeInterval));
+
+      // Add the status to a holdsStatusInTime anonymous individual.
+      axioms.add(
+          df.getOWLAnnotationAssertionAxiom(
+              df.getOWLAnnotationProperty(IRI.create("http://purl.org/spar/pso/withStatus")),
+              holdsStatusInTime,
+              status));
+
+      // Annotate the phyloref with the holdsStatusInTime anonymous individual.
+      axioms.add(
+          df.getOWLAnnotationAssertionAxiom(
+              df.getOWLAnnotationProperty(IRI.create("http://purl.org/spar/pso/holdsStatusInTime")),
+              phylorefIRI,
+              holdsStatusInTime));
+
+      // Return the axioms that need to be added.
+      return axioms;
     }
 
     @Test
@@ -72,6 +167,14 @@ class PhylorefHelperTest {
 
       Set<OWLNamedIndividual> phylorefs =
           PhylorefHelper.getPhyloreferencesWithoutReasoning(testOntology);
+      assertEquals(1, phylorefs.size());
+      assertTrue(
+          phylorefs.contains(phyloref),
+          "Phyloref 'phyloref1' has been retrieved without reasoning");
+
+      // Calling getPhyloreferences() with a null reasoner should also return
+      // the same results.
+      phylorefs = PhylorefHelper.getPhyloreferences(testOntology, null);
       assertEquals(1, phylorefs.size());
       assertTrue(
           phylorefs.contains(phyloref),
@@ -94,6 +197,54 @@ class PhylorefHelperTest {
       assertEquals(1, phylorefs.size());
       assertTrue(
           phylorefs.contains(phyloref), "Phyloref 'phyloref1' has been retrieved with reasoning");
+    }
+
+    @Test
+    @DisplayName("can retrieve current statuses for a particular phyloreference")
+    void canRetrieveStatuses() throws OWLOntologyStorageException {
+      OWLDataFactory df = ontologyManager.getOWLDataFactory();
+      OWLNamedIndividual phyloref =
+          df.getOWLNamedIndividual(IRI.create("http://example.org/phyloref1"));
+
+      // Uncomment the next line to provide a copy of the test ontology for debugging.
+      // testOntology.saveOntology(new FileDocumentTarget(new File("./output.txt")));
+
+      List<PhylorefStatus> statuses = PhylorefHelper.getStatusesForPhyloref(phyloref, testOntology);
+      assertEquals(4, statuses.size());
+
+      // Note that OWL doesn't really have a concept of a list of statuses;
+      // we have no guarantee as to which order these statuses will appear in.
+
+      // Find the first status somewhere in this list.
+      assertEquals(
+          1,
+          statuses
+              .stream()
+              .filter(
+                  st ->
+                      st.getPhyloref().equals(phyloref)
+                          && st.getStatus().equals(IRI.create("http://purl.org/spar/pso/draft"))
+                          && st.getIntervalStart().equals(Instant.parse("2018-11-14T01:00:00.00Z"))
+                          && st.getIntervalEnd().equals(Instant.parse("2018-11-14T02:00:00.00Z")))
+              .count(),
+          "Found the first status: 'pso:draft'");
+
+      // Find the final status somewhere in this list.
+      assertEquals(
+          1,
+          statuses
+              .stream()
+              .filter(
+                  st ->
+                      st.getPhyloref().equals(phyloref)
+                          && st.getStatus().equals(IRI.create("http://purl.org/spar/pso/published"))
+                          && st.getIntervalStart().equals(Instant.parse("2018-11-14T04:00:00.00Z"))
+                          && st.getIntervalEnd() == null)
+              .count(),
+          "Found the final status: 'pso:published'");
+
+      // How many are "current" (i.e. missing an end time)?
+      assertEquals(1, statuses.stream().filter(st -> st.getIntervalEnd() == null).count());
     }
   }
 }
