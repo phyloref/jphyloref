5b0082528e533f52a165036694f59dc8f3675f9a
Gaurav Vaidya <gaurav@ggvaidya.com>
2018-12-13 19:09:01 -0500

    Added test for reading JSONLD over STDIN using the special filename '-'.

--- src/test/java/org/phyloref/jphyloref/TestCommandTest.java
+++ src/test/java/org/phyloref/jphyloref/TestCommandTest.java
@@ -2,9 +2,14 @@ package org.phyloref.jphyloref;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -119,6 +124,61 @@ class TestCommandTest {
           "1..1\nok 1 Phyloreference '1'\n# The following nodes were matched and expected this phyloreference: [1]\n\n",
           outputStr);
       resetIO();
+    }
+
+    @Test
+    @DisplayName("can load JSON-LD files from STDIN")
+    void testJSONLDFromStdin() {
+      // The test file we're going to use in this test.
+      String testFilename = "src/test/resources/phylorefs/dummy1.jsonld";
+
+      // We're going to take over STDIN, so let's save the real STDIN so we can
+      // restore it.
+      InputStream actualStdin = System.in;
+
+      try {
+        // Reset STDOUT and STDERR and set STDIN.
+        resetIO();
+
+        // Create an InputStream with the contents of the JSON-LD file we've
+        // already tested, and set the System STDIN to point to that input stream.
+        ByteArrayInputStream input =
+            new ByteArrayInputStream(Files.readAllBytes(Paths.get(testFilename)));
+        System.setIn(input);
+
+        // Test the contents of STDIN.
+        int exitCode = jphyloref.execute(new String[] {"test", "--jsonld", "-"});
+
+        // Obtain STDOUT and STDERR.
+        String outputStr = output.toString("UTF-8");
+        String errorStr = error.toString("UTF-8");
+
+        // Test whether the exit code, STDERR and STDOUT is as expected.
+        assertEquals(0, exitCode);
+        assertTrue(
+            errorStr.contains("Input: -"),
+            "Make sure JPhyloRef reports the name of the file being processed");
+        assertTrue(
+            errorStr.endsWith(
+                "Testing complete:1 successes, 0 failures, 0 failures marked TODO, 0 skipped.\n"),
+            "Make sure the testing was successful.");
+        assertEquals(
+            "1..1\nok 1 Phyloreference '1'\n# The following nodes were matched and expected this phyloreference: [1]\n\n",
+            outputStr);
+      } catch (UnsupportedEncodingException ex) {
+        // Could be thrown when converting STDOUT/STDERR to String as UTF-8.
+        throw new RuntimeException("'UTF-8' is not supported as an encoding: " + ex);
+      } catch (IOException ex) {
+        // Could be thrown if there were errors reading the testing file.
+        throw new RuntimeException(
+            "Expected testing file '" + testFilename + "' not found or could not be read: " + ex);
+      } finally {
+        // Reset System.in to the actual STDIN.
+        System.setIn(actualStdin);
+
+        // Reset everything else as well.
+        resetIO();
+      }
     }
   }
 }
