b9b6ff5490e86b22f78555efe922f7bd94e7cb73
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-04-15 02:38:12 -0400

    Updated version to 1.0.0.

--- src/main/java/org/phyloref/jphyloref/JPhyloRef.java
+++ src/main/java/org/phyloref/jphyloref/JPhyloRef.java
@@ -27,7 +27,7 @@ public class JPhyloRef {
   private static final Logger logger = LoggerFactory.getLogger(JPhyloRef.class);
 
   /** Version of JPhyloRef. */
-  public static final String VERSION = "0.4.0";
+  public static final String VERSION = "1.0.0";
 
   /** List of all commands included in JPhyloRef. */
   private List<Command> commands =
