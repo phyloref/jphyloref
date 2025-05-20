3966fbdc68658b6c6fc6ee96e81ce42c5bd618e4
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-05-20 00:47:31 -0400

    Improved documentation and modified command.

--- webhook/start.sh
+++ webhook/start.sh
@@ -1,1 +1,6 @@
-./webhook -port $PORT -hooks hooks.json -header Access-Control-Allow-Origin=* -header Access-Control-Allow-Headers=x-hub-signature -verbose
+# Start a Webhook instance to respond to queries.
+#
+# Environmental variables:
+# - PORT: The HTTP port to listen on (e.g. 80)
+
+webhook -port "${PORT:=80}" -hooks hooks.json -header Access-Control-Allow-Origin=* -header Access-Control-Allow-Headers=x-hub-signature -verbose
